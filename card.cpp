697956841cbfbb8285655c398e94552448ac1dda
Hitonoriol <gorgilike@mail.ru>
2023-04-11 19:17:00 +0300

    Card visibility: if card is not visible, its backside is displayed
    + Rename `Card::Value` -> `Card::Rank`
    + Getters for card's rank and suit
    + Static conversion functions between suit / rank indices and enums

--- card.cpp
+++ card.cpp
@@ -4,16 +4,21 @@
 
 #include <QtMath>
 
-Card::Card(Suit suit, Value value)
-    : suit(suit), value(value) {}
+Card::Card(Suit suit, Rank value)
+    : suit(suit), rank(value) {}
+
+Card::Card(size_t suitIdx, size_t rankIdx)
+    : Card(toSuit(suitIdx), toRank(rankIdx)) {}
 
 void Card::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
+    auto rank_idx = visible ? toIdx(rank) : 0u;
+    auto suit_idx = toIdx(visible ? suit : Suit::HIDDEN);
     Resources::cardSheet->paintRegion(
         painter,
         pos(),
-        static_cast<size_t>(value),
-        static_cast<size_t>(suit)
+        rank_idx,
+        suit_idx
     );
     Q_UNUSED(option)
     Q_UNUSED(widget)
@@ -24,8 +29,41 @@ QRectF Card::boundingRect() const
     return Resources::cardSheet->boundingRect();
 }
 
-size_t Card::getValue()
+size_t Card::getScore()
 {
-    return qMax(10u, static_cast<size_t>(value) + 2);
+    if (rank == Rank::ACE)
+        return 11u;
+
+    return qMax(10u, toIdx(rank) + 2);
+}
+
+Card::Rank Card::getRank()
+{
+    return rank;
+}
+
+Card::Suit Card::getSuit()
+{
+    return suit;
+}
+
+bool Card::isVisible()
+{
+    return visible;
+}
+
+void Card::setVisible(bool value)
+{
+    visible = value;
+}
+
+void Card::hide()
+{
+    setVisible(false);
+}
+
+void Card::show()
+{
+    setVisible(true);
 }
 
--- card.h
+++ card.h
@@ -8,7 +8,7 @@
 class Card : public QGraphicsItem
 {
 public:
-    enum class Value
+    enum class Rank
     {
         TWO,
         THREE,
@@ -30,20 +30,63 @@ public:
         HEARTS,
         DIAMONDS,
         CLUBS,
-        SPADES
+        SPADES,
+        HIDDEN
     };
 
 private:
     Suit suit;
-    Value value;
-
-    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
-    QRectF boundingRect() const;
+    Rank rank;
+    bool visible = true;
 
 public:
-    Card(Suit suit, Value value);
+    Card(Suit, Rank);
+    Card(size_t suitIdx, size_t rankIdx);
 
-    size_t getValue();
+    /* Get this card's score worth. Always 11 for Ace. */
+    size_t getScore();
+
+    Rank getRank();
+    Suit getSuit();
+
+    /* Get this card's visibility state - if it's not visible, the backside is displayed. */
+    bool isVisible();
+
+    void setVisible(bool);
+    void hide();
+    void show();
+
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+    QRectF boundingRect() const override;
+
+    static constexpr size_t SUITS = static_cast<size_t>(Suit::SPADES) + 1;
+    static constexpr size_t RANKS = static_cast<size_t>(Rank::ACE) + 1;
+
+    inline static Rank toRank(size_t rank_idx)
+    {
+        if (rank_idx >= RANKS)
+            throw std::runtime_error("Invalid rank index");
+
+        return static_cast<Rank>(rank_idx);
+    }
+
+    inline static Suit toSuit(size_t suit_idx)
+    {
+        if (suit_idx >= SUITS)
+            throw std::runtime_error("Invalid suit index");
+
+        return static_cast<Suit>(suit_idx);
+    }
+
+    inline static size_t toIdx(Suit suit)
+    {
+        return static_cast<size_t>(suit);
+    }
+
+    inline static size_t toIdx(Rank rank)
+    {
+        return static_cast<size_t>(rank);
+    }
 };
 
 #endif // CARD_H
