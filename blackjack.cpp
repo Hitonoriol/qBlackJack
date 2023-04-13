a62ad94adefbdcc00458ed3929f4fc25d07b562f
Hitonoriol <gorgilike@mail.ru>
2023-04-13 21:11:01 +0300

    Fully implement BlackJack game logic

--- blackjack.cpp
+++ blackjack.cpp
@@ -3,27 +3,35 @@
 
 #include <QMessageBox>
 
-BlackJack::BlackJack(Scene &scene)
-    : dealer(new Hand), player(new Hand)
-{
-    scene.add(dealer);
-    scene.add(player);
-}
+BlackJack::BlackJack()
+    : dealer(new Hand), player(new Hand) {}
 
-void BlackJack::placeBet(int amount)
+bool BlackJack::placeBet(int amount)
 {
-    if (amount > balance || amount <= 0) {
-        QMessageBox::warning(
-            nullptr, "Error",
-            "Bet amount is invalid!",
-            QMessageBox::Ok
-        );
-        return;
-    }
+    if (amount > balance || amount <= 0)
+        return false;
 
     balance -= amount;
     bet = amount;
     startGame();
+    return true;
+}
+
+void BlackJack::hit()
+{
+    player->draw(deck);
+    auto score = player->evaluate();
+    qDebug() << "Drew a card worth" << player->lastCard().getScore() << "; Total:" << score;
+    if (score > BJ)
+        state = GameState::Bust;
+}
+
+void BlackJack::stand()
+{
+    dealer->lastCard().show();
+    while (dealer->evaluate() < dealerThreshold)
+        dealer->draw(deck);
+    checkGameEndConditions();
 }
 
 int BlackJack::getBalance()
@@ -31,8 +39,51 @@ int BlackJack::getBalance()
     return balance;
 }
 
+int BlackJack::getBet()
+{
+    return bet;
+}
+
+BlackJack::GameState BlackJack::getGameState()
+{
+    return state;
+}
+
+bool BlackJack::isGameInProgress()
+{
+    return state == GameState::InProgress;
+}
+
+int BlackJack::winAmount()
+{
+    switch(state) {
+    case GameState::BlackJack:
+        return bet * 2.5;
+
+    case GameState::Win:
+        return bet * 2;
+
+    case GameState::Push:
+        return bet;
+
+    default:
+        return 0;
+    }
+}
+
+Hand &BlackJack::getDealerHand()
+{
+    return *dealer;
+}
+
+Hand &BlackJack::getPlayerHand()
+{
+    return *player;
+}
+
 void BlackJack::startGame()
 {
+    state = GameState::InProgress;
     deck.reset();
     dealer->clear();
     player->clear();
@@ -40,5 +91,32 @@ void BlackJack::startGame()
     player->draw(deck);
     dealer->draw(deck);
     dealer->lastCard().hide();
+
+    if (player->evaluate() == BJ) {
+        state = GameState::BlackJack;
+        balance += winAmount();
+    }
+}
+
+void BlackJack::checkGameEndConditions()
+{
+    auto playerScore = player->evaluate();
+    auto dealerScore = dealer->evaluate();
+
+    qDebug() << "Ending the game. Dealer has:" << dealerScore << "; player has:" << playerScore;
+
+    if (playerScore > BJ)
+        state = GameState::Bust;
+
+    else if (dealerScore > BJ || dealerScore < playerScore)
+        state = GameState::Win;
+
+    else if (dealerScore > playerScore)
+        state = GameState::Loss;
+
+    else if (dealerScore == playerScore)
+        state = GameState::Push;
+
+    balance += winAmount();
 }
 
--- blackjack.h
+++ blackjack.h
@@ -4,33 +4,63 @@
 #include "deck.h"
 #include "hand.h"
 
-#include "scene.h"
-
 class BlackJack
 {
+public:
+    enum class GameState
+    {
+        None,       // Only used as initial `state` value before the first game
+        InProgress,
+        Bust,       // When player's total is >21
+        Loss,       // When dealer has a better hand
+        Win,        // When player has a better hand
+        BlackJack,  // When player's two initial cards are worth 21
+        Push        // When player and dealer have equivalent hands
+    };
+
 private:
+    GameState state = GameState::None;
     Deck deck;
 
     /* Dealer's & player's hands.
      * Safe to allocate via `new` because they will be owned by `QGraphicsScene`,
-     * not by this class. */
+     * not by this class.
+     *
+     * TODO: Make hands and cards non-Qt objects and create separate
+     * `HandView` and `CardView` classes for displaying them graphically.
+     */
     Hand *dealer;
     Hand *player;
 
     int balance = 100;
-    int bet;
+    int bet = 0;
+
+    /* Score after which dealer stops drawing cards from the deck */
+    size_t dealerThreshold = 17;
 
     void startGame();
+    void checkGameEndConditions();
 
 public:
-    BlackJack(Scene&);
+    BlackJack();
 
-    void placeBet(int amount);
+    bool placeBet(int amount);
 
     void hit();
     void stand();
 
     int getBalance();
+    int getBet();
+
+    GameState getGameState();
+    bool isGameInProgress();
+
+    int winAmount();
+
+    Hand& getDealerHand();
+    Hand& getPlayerHand();
+
+    static constexpr size_t BJ = 21;
 };
 
 #endif // BLACKJACK_H
